123b7bc1e54eccc4a8f575890e8c1b03f6dd4d3b
sarthak0401 <sarthakbokade413@gmail.com>
2024-09-11 01:02:42 +0530

    dockerised the mern todo application

--- frontend/todo/vite.config.js
+++ frontend/todo/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // This binds the server to all network interfaces
+    port: 5173,       // Ensure the port matches the one exposed in Docker
+  },
 })
